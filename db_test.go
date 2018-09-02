27664565443084564bcc46aec5fed983b89cebf8
chyeh <chyeh.taiwan@gmail.com>
2018-09-02 22:05:50 +0300

    Data layer tests for updating a recipe

--- db_test.go
+++ db_test.go
@@ -19,7 +19,6 @@ var _ = Describe("Testing database object", func() {
 		CREATE DATABASE test_hellofresh
 		`)
 	})
-
 	AfterEach(func() {
 		testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/?sslmode=disable")
 		defer testDB.close()
@@ -62,7 +61,6 @@ var _ = Describe("Testing database object", func() {
 			Expect(actual).NotTo(BeNil())
 			Expect(actual).To(HaveLen(0))
 		})
-
 		It("lists non-empty table", func() {
 			testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/test_hellofresh?sslmode=disable")
 			defer testDB.close()
@@ -79,11 +77,9 @@ var _ = Describe("Testing database object", func() {
 				Name:         null.NewString("name3", true),
 				IsVegetarian: null.NewBool(false, true),
 			})
-
 			Expect(testDB.listRecipes()).To(HaveLen(3))
 		})
 	})
-
 	Context("adding a new recipe", func() {
 		BeforeEach(func() {
 			testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/test_hellofresh?sslmode=disable")
@@ -133,7 +129,58 @@ var _ = Describe("Testing database object", func() {
 			Expect(actual.Difficulty.Int64).To(Equal(int64(4)))
 			Expect(actual.IsVegetarian).To(BeFalse())
 			Expect(testDB.listRecipes()).To(HaveLen(2))
-
 		})
+	})
+	Context("updating a recipe", func() {
+		BeforeEach(func() {
+			testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/test_hellofresh?sslmode=disable")
+			defer testDB.close()
+
+			testDB.sqlxDB.MustExec(`
+			DROP TABLE IF EXISTS recipe
+			`)
+			testDB.sqlxDB.MustExec(`
+			CREATE TABLE recipe(
+				r_id SERIAL PRIMARY KEY,
+				r_name VARCHAR(512) NOT NULL,
+				r_prep_time SMALLINT,
+				r_difficulty SMALLINT,
+				r_vegetarian BOOLEAN NOT NULL
+			)
+			`)
+		})
+		AfterEach(func() {
+			testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/test_hellofresh?sslmode=disable")
+			defer testDB.close()
+
+			testDB.sqlxDB.MustExec(`
+			DROP TABLE recipe
+			`)
+		})
+		It("updates a existent record in the recipe table", func() {
+			testDB := newSqlxPostgreSQL("postgres://hellofresh:hellofresh@localhost:5432/test_hellofresh?sslmode=disable")
+			defer testDB.close()
+
+			testDB.addRecipe(&PostRecipeArg{
+				Name:         null.NewString("name2", true),
+				PrepareTime:  null.NewInt(2, true),
+				Difficulty:   null.NewInt(4, true),
+				IsVegetarian: null.NewBool(false, true),
+			})
+			testDB.updateRecipe(&Recipe{
+				ID:           1,
+				Name:         "name2_updated",
+				PrepareTime:  null.NewInt(3, true),
+				Difficulty:   null.NewInt(4, true),
+				IsVegetarian: false,
+			})
+
+			actual := testDB.getRecipeByID(1)
+			Expect(actual.Name).To(Equal("name2_updated"))
+			Expect(actual.PrepareTime.Int64).To(Equal(int64(3)))
+			Expect(actual.Difficulty.Int64).To(Equal(int64(4)))
+			Expect(actual.IsVegetarian).To(BeFalse())
+		})
+
 	})
 })
