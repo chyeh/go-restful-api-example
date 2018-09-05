adda466b0a046b3fac7c7349650e34c408e9f007
chyeh <chyeh.taiwan@gmail.com>
2018-09-05 19:32:56 +0300

    Code refinement

--- model.go
+++ model.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 	null "gopkg.in/guregu/null.v3"
 )
 
@@ -71,4 +74,90 @@ func (a *PostRateRecipeArg) updateRecipe(r *Recipe) {
 	}
 	r.Rating.Float64 = ((r.Rating.Float64 * float64(r.RatedNum.Int64)) + float64(a.Rating.Int64)) / float64(r.RatedNum.Int64+1)
 	r.RatedNum.Int64++
+}
+
+type filter struct {
+	isSet          bool
+	name           null.String
+	prepTimeFrom   null.Int
+	prepTimeTo     null.Int
+	difficultyFrom null.Int
+	difficultyTo   null.Int
+	isVegetarian   null.Bool
+}
+
+func newFilter(c *gin.Context) *filter {
+	var f filter
+	if v, ok := c.GetQuery("name"); ok {
+		f.name = null.StringFrom(v)
+		f.isSet = true
+	}
+	if v, ok := c.GetQuery("prepare_time_from"); ok {
+		n, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		f.prepTimeFrom = null.IntFrom(n)
+		f.isSet = true
+	}
+	if v, ok := c.GetQuery("prepare_time_to"); ok {
+		n, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		f.prepTimeTo = null.IntFrom(n)
+		f.isSet = true
+	}
+	if v, ok := c.GetQuery("difficulty_from"); ok {
+		n, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		f.difficultyFrom = null.IntFrom(n)
+		f.isSet = true
+	}
+	if v, ok := c.GetQuery("difficulty_to"); ok {
+		n, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		f.difficultyTo = null.IntFrom(n)
+		f.isSet = true
+	}
+	if v, ok := c.GetQuery("is_vegetarian"); ok {
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			panic(err)
+		}
+		f.isVegetarian = null.BoolFrom(b)
+		f.isSet = true
+	}
+	return &f
+}
+
+func (f *filter) whereClause() string {
+	if !f.isSet {
+		return ""
+	}
+	clause := " WHERE "
+	if f.name.Valid {
+		clause += (" r_name LIKE '%" + f.name.String + "%' AND ")
+	}
+	if f.prepTimeFrom.Valid {
+		clause += (" r_prep_time >= " + strconv.Itoa(int(f.prepTimeFrom.Int64)) + " AND ")
+	}
+	if f.prepTimeTo.Valid {
+		clause += (" r_prep_time <= " + strconv.Itoa(int(f.prepTimeTo.Int64)) + " AND ")
+	}
+	if f.difficultyFrom.Valid {
+		clause += (" r_difficulty >= " + strconv.Itoa(int(f.difficultyFrom.Int64)) + " AND ")
+	}
+	if f.difficultyTo.Valid {
+		clause += (" r_difficulty <= " + strconv.Itoa(int(f.difficultyTo.Int64)) + " AND ")
+	}
+	if f.isVegetarian.Valid {
+		clause += (" r_vegetarian = " + strconv.FormatBool(f.isVegetarian.Bool) + " AND ")
+	}
+	clause += " true "
+	return clause
 }
